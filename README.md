90aa360b6d0ae38545764ab8d9cae2eeb33de186
kamrenz <kamren@developintelligence.com>
2015-06-24 11:31:09 -0600

    Adding files needed for grid stripping

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # bootstrap-grid
+Who needs all of bootstrap? A lot of times I just want to work with its grid. Here is the grid available in CSS and Less.
---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+/**
+ * Grunt file for handling Less transpilation
+ * @param grunt
+ */
+module.exports = function(grunt) {
+
+    // Load Grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        //Grunt task configuration
+        // Transpile our less code
+        less: {
+            /**
+             * Transpiles each individual Less component.
+             *   This isn't needed unless you want to see each component converted directly.
+             **/
+            components: {
+                expand: true,
+                src: 'src/css/less-component/**/*.less',
+                ext: '.css'
+            },
+            /**
+             * Creates a finalized complete CSS for development.
+             *   This brings in all the bootstrap components and generates one CSS file to work with.
+             */
+
+            main: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'src/css/main.css': 'src/css/main.less'
+                }
+            }
+        },
+
+        //Live watching
+        watch: {
+            scripts: {
+                files: [
+                    'src/css/main.less'
+                ],
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    // Define the default task with array of Grunt tasks
+    grunt.registerTask('default', ['less']);
+};
